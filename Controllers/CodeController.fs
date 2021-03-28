83f5889c09f644a76decf65733f0e3ad092e8819
Lilian VERLHAC <lilian.verlhac@epitech.eu>
2021-03-29 01:43:26 +0200

    ADD Lexer and Architecture

---
+++ Controllers/CodeController.fs
@@ -0,0 +1,22 @@
+﻿namespace lerve.Controllers
+
+open System
+open FSharp.Json
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open lerve.Services.LispService
+
+[<ApiController>]
+[<Route("[controller]")>]
+type CodeController (logger : ILogger<CodeController>) =
+    inherit ControllerBase()
+    
+    [<HttpGet>]
+    member _.Get() =
+        match lexer """aazeazezaea (define toto a b (a + b))""" with
+        | Ok(tokens) -> ActionResult<obj>(base.Ok(Json.serialize tokens))
+        | Error(err)  ->  ActionResult<obj>(base.BadRequest err) 
+    
+    [<HttpPost>]
+    member _.Post() = "titi"
+    
